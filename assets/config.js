accd185a5883864e27c3d15a00d7783424cc5fdb
ivankolt <va.koltsov@mail.ru>
2026-08-17 11:45:25 +0500

    Migrate website to totalcode.ru

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
-// Укажите числовой ID счетчика после создания счетчика для total-code.ru.
+// Укажите числовой ID счетчика после создания счетчика для totalcode.ru.
 window.TOTAL_CODE_CONFIG = {
   yandexMetrikaId: ""
 };
